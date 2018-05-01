6d3cd706de08a29a40e1ac8d439b6a841833b469
caek88 <35225849+caek88@users.noreply.github.com>
2018-05-01 15:36:35 -0400

    Update [0.0.0].cpp
    Added a function under driveMethods class with driving algorithm

--- [0.0.0].cpp
+++ [0.0.0].cpp
@@ -1,4 +1,67 @@
-class driveMethods {}
+class driveMethods {
+    public:
+        void drive(int x, int y){
+            //Deadzone Filter to prevent motor whining or drifting
+            if (y < 10 && y > -10) {
+                y = 0;
+            }
+            if (x < 10 && x > -10) {
+                x = 0;
+            }
+
+            //Left Motor Calculator
+            if (y >= 0) {
+                if (x >= 0) {
+                    if (y >= x) {
+                        powerLeft = y;
+                    } else {
+                        powerLeft = x;
+                    }
+                } else {
+                    powerLeft = y + x;
+                }
+            } else {
+                if (x <= 0) {
+                    if (y <= x) {
+                        powerLeft = y;
+                    } else {
+                        powerLeft = x;
+                    }
+                } else {
+                    powerLeft = y + x;
+                }
+            }
+
+            //Right Motor Calculator
+            if (y >= 0) {
+                if (x <= 0) {
+                    if (y >= -x) {
+                        powerRight = y;
+                    } else {
+                        powerRight = -x;
+                    }
+                } else {
+                    powerRight = y - x;
+                }
+            } else {
+                if (x >= 0) {
+                    if (y <= -x) {
+                        powerRight = y;
+                    } else {
+                        powerRight = -x;
+                    }
+                } else {
+                    powerRight = y - x;
+                }
+            }
+
+            //Use power variables to control the motors
+            motor[leftFront] = powerLeft;
+            motor[rightBack] = powerRight;
+            motor[leftBack] = powerLeft;
+            motor[rightFront] = powerRight;
+        }
+}
 class pid: public driveMethods {
     public:
         struct pidCalcReturn {
