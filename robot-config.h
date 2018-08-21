e9a44b98d3f667ea9a80a8b8b5c299a01a1a0271
caek88 <35225849+caek88@users.noreply.github.com>
2018-08-21 13:18:50 -0400

    Update robot-config.h
    Fixed errors in compilation

--- robot-config.h
+++ robot-config.h
@@ -1,7 +1,7 @@
 //robot-config.h
 
-vex::brain = robotMain;
-vex::controller ctrPrimary = vex::controller(primary);
+vex::brain robotMain;
+vex::controller ctrPrimary = vex::controller(vex::controllerType::primary);
 vex::competition compControl;
 vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
 vex::motor mtrDriveRight = vex::motor(vex::PORT10);
