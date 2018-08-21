2cfe36b83396c1aa5a56470b85f3c13721bba93b
caek88 <35225849+caek88@users.noreply.github.com>
2018-08-21 12:51:50 -0400

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -3,3 +3,5 @@
 vex::brain = robotMain;
 vex::controller ctrPrimary = vex::controller(primary);
 vex::competition compControl;
+vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
+vex::motor mtrDriveRight = vex::motor(vex::PORT10);
