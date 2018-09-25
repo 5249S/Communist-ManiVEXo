4245fa04875cc30c2cabdb5defd18b562f057763
caek88 <35225849+caek88@users.noreply.github.com>
2018-09-25 17:00:17 -0400

    Formatted the ball launcher program

--- Ball Launcher [0.1.0].cpp
+++ Ball Launcher [0.1.0].cpp
@@ -1,3 +1,26 @@
-class flag {
-
-}
+class Flag {
+  private:
+    const float GRAVITY = 9.8 //m/s^2
+    const float INITIAL_VELOCITY = 0 //m/s
+    float distance = 0;
+    float height = 0;
+  public:
+    void calculateRequiredAngle(float alpha, float beta){
+      
+    }
+    void checkForHit(float alpha, float beta){
+      
+    }
+};
+class Launcher {
+  private:
+    Flag htzFlags[9];
+    
+  public:
+    void scanForFlags(){
+      
+    }
+    void targetSpecificFlag(){
+      
+    }
+};
