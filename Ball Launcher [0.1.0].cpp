69eeb2fede69383eff41262c45eb69261c03bc8f
caek88 <35225849+caek88@users.noreply.github.com>
2018-09-25 17:25:14 -0400

    Update Ball Launcher [0.1.0].cpp
    started implementing calculations

--- Ball Launcher [0.1.0].cpp
+++ Ball Launcher [0.1.0].cpp
@@ -2,20 +2,41 @@ class Flag {
   private:
     const float GRAVITY = 9.8 //m/s^2
     const float INITIAL_VELOCITY = 0 //m/s
+    const float FLAG_HEIGHT = 0 //meters
     float distance = 0;
     float height = 0;
-  public:
-    void calculateRequiredAngle(float alpha, float beta){
-      
+    void calculateDistance(double alpha, double beta){
+      distance = (float)(FLAG_HEIGHT/(tan(beta)-tan(alpha)));
+      height = distance * (float)(tan(alpha));
     }
-    void checkForHit(float alpha, float beta){
+  public:
+    float calculateRequiredAngle(float alpha, float beta){
+      float v = INITIAL_VELOCITY;
+      float g = GRAVITY;
+      float h = height + FLAG_HEIGHT/2;
+      return (float)(atan((v^2 - sqrt(v^4-g(g*distance^2-2*h*v^2)))/g*distance));
+    }
+    bool checkForHit(float alpha, float beta){
       
     }
 };
 class Launcher {
   private:
+    const double pi = 3.141592;
+    double toRad(double degrees){
+      return degrees * (pi/180.0);
+    }
+    double toDeg(double radians){
+      return radians * (180.0/pi);
+    }
     Flag htzFlags[9];
-    
+    const float FOV = 47.0;//field of view
+    const float FOCAL_LENGTH = 200/tan(toRad(23.5));
+    float angleAtPoint(int y){
+      float offset = 0;
+      int yP = y - 200;
+      return (float)(atan((double)(yP/FOCAL_LENGTH)) + offset;
+    }
   public:
     void scanForFlags(){
       
