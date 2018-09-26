55978436cfb41713abad8dbfcafea967c3150439
caek88 <35225849+caek88@users.noreply.github.com>
2018-09-26 19:31:08 -0400

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -5,3 +5,7 @@ vex::controller ctrPrimary = vex::controller(vex::controllerType::primary);
 vex::competition compControl;
 vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
 vex::motor mtrDriveRight = vex::motor(vex::PORT10);
+
+vex::vision visLauncher = vex::vision(2);
+vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.B);
+vex::accelerometer = 
