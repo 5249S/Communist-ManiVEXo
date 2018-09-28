e177dc6a014e3da877be9f2a6d377671cbe0fdb5
caek88 <35225849+caek88@users.noreply.github.com>
2018-09-28 16:20:01 -0400

    Update Ball Launcher [0.1.0].cpp
    Finished SFF, starting TSF

--- Ball Launcher [0.1.0].cpp
+++ Ball Launcher [0.1.0].cpp
@@ -5,25 +5,27 @@ class Flag {
         const float FLAG_HEIGHT = 0 //meters
         float distance = 0;
         float height = 0;
+        
+    public:
         void calculateDistance(double alpha, double beta){
             int offset = 0;
             distance = (float)(FLAG_HEIGHT/(tan(beta)-tan(alpha)) + offset);
             height = distance * (float)(tan(alpha));
         }
-    public:
-        float calculateRequiredAngle(float alpha, float beta){
+        float calculateRequiredAngle(){
             float v = INITIAL_VELOCITY;
             float g = GRAVITY;
             float h = height + FLAG_HEIGHT/2;
             int offset = 0;
             return (float)(atan((v^2 - sqrt(v^4-g(g*distance^2-2*h*v^2)))/g*distance) + offset);
         }
-        bool checkForHit(float alpha, float beta){
+        bool checkForHit(){
             
         }
 };
 class Launcher {
-    private:
+    private:        
+        int htzIndex = 0;
         const double pi = 3.141592;
         double toRad(double degrees){
             return degrees * (pi/180.0);
@@ -35,28 +37,40 @@ class Launcher {
         const float FOV = 47.0;//field of view
         const float FOCAL_LENGTH = 200/tan(toRad(23.5));
         const int htzMax = 340;
-        const int htzMin = 300
+        const int htzMin = 300;
         float angleAtPoint(int y){
             float offset = 0;
             int yP = y - 200;
             return (float)(atan((double)(yP/FOCAL_LENGTH)) + offset;
         }
+        Pid pidLauncher;
+        void runPid(float angle){
+            
+        }
     public:
+        int flagX[9];
         void scanForFlags(){
             if (colorRed){
-                visLauncher.takeSnapshot(SIG_RED_FLAG);
-            } else {
                 visLauncher.takeSnapshot(SIG_BLUE_FLAG);
+            } else {
+                visLauncher.takeSnapshot(SIG_RED_FLAG);
             }
-            int htzIndex = 0;
-            for (int i = 0, i < visLauncher.objectCount, i++){
+            htzIndex = 0;
+            for (int i = 0; i < visLauncher.objectCount; i++){
                 if (visLauncher.objects[i].centerX < htzMax && visLauncher.objects[i].centerX > htzMin){
-                    float alpha = angleAtPoint(visLauncher.objects[i].
-                    htzFlags[htzIndex].
+                    float beta = angleAtPoint(visLauncher.objects[i].originY);
+                    float alpha = angleAtPoint(visLauncher.objects[i].originY - 2 * (visLauncher.objects[i].originY - visLauncher.objects[i].centerY));
+                    htzFlags[htzIndex].calculateDistance(alpha, beta);
+                    htzIndex ++;
                 }
+                flagX[i] = visLauncher.objects[i].centerX;
             }
         }
         void targetSpecificFlag(){
-
+            smallestAngleDifference = 100;
+            current 
+            for (int i = 0; i < htzIndex; i++){
+                
+            }
         }
 };
