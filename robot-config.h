495f509177a900ed8efb697fcd678b846046c168
caek88 <35225849+caek88@users.noreply.github.com>
2018-09-30 14:21:49 -0400

    Fixed syntax

--- robot-config.h
+++ robot-config.h
@@ -6,9 +6,9 @@ vex::competition compControl;
 
 vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
 vex::motor mtrDriveRight = vex::motor(vex::PORT10);
-vex::gyro gyroNav = vex::gyro::(robotMain.ThreeWirePort.A);
+vex::gyro gyroNav = vex::gyro(robotMain.ThreeWirePort.A);
 
-vex::vision visLauncher = vex::vision(vex::PORT::2);
+vex::vision visLauncher = vex::vision(vex::PORT2);
 vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherZ = vex::accelerometer(robotMain.ThreeWirePort.C);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.D);
