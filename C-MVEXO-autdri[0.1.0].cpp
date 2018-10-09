5939a1ac8666954d3793924b7025b08059c323d5
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-09 16:43:07 -0400

    Update C-MVEXO-autdri[0.1.0].cpp

--- C-MVEXO-autdri[0.1.0].cpp
+++ C-MVEXO-autdri[0.1.0].cpp
@@ -23,6 +23,10 @@ void driver(){
     //Declare variables here
     while (confirmDriver()){
         //Run driver implementation here
+        int y = ctrPrimary.Axis3.position(vex::percentUnits::pct);
+        int x = ctrPrimary.Axis4.position(vex::percentUnits::pct);
+        robot.driveH(y, x);
+        wait(20);
         if (mode == 3){
             //Run something for running in test mode
         }
