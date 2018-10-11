013103264a0fd39e0448f66e55416e1fe31fc89f
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-11 16:29:17 -0400

    Update C-MVEXO-main [0.1.0].cpp

--- C-MVEXO-main [0.1.0].cpp
+++ C-MVEXO-main [0.1.0].cpp
@@ -30,13 +30,13 @@ void wait(int time){
 GyroSettings gyroLauncherSet;
 GyroSettings gyroNavSet;
 void calibrateGyros(){//Calibrates gyros
-    ctrPrimary.Screen.clearScreen();
-    ctrPrimary.Screen.setCursor(0,0);
-    ctrPrimary.Screen.print("Gyros Calibrating");
-    ctrPrimary.Screen.newLine();
-    ctrPrimary.Screen.print("Do Not Touch Robot");
-    ctrPrimary.Screen.newLine();
-    ctrPrimary.Screen.print("(B) Bypass");
+    robotMain.Screen.clearScreen();
+    robotMain.Screen.setCursor(0,0);
+    robotMain.Screen.print("Gyros Calibrating");
+    robotMain.Screen.newLine();
+    robotMain.Screen.print("Do Not Touch Robot");
+    robotMain.Screen.newLine();
+    robotMain.Screen.print("(B) Bypass");
     gyroNav.startCalibration();
     gyroLauncher.startCalibration();
     while(gyroNav.isCalibrating() || gyroLauncher.isCalibrating()){//waits for both gyros to finish 
@@ -57,94 +57,96 @@ void stopAllMotors(){
     mtrLiftLeft.stop(vex::brakeType::coast);
     mtrLiftRight.stop(vex::brakeType::coast);
     
-}
+}     
 class DisplaySelection {//Class created to hold and change the values needed to move the display up and down
-    private:
-        int maxLines = 3;
-        int current = 0;
-        int topLine = 0;
-        bool selectionMade = false;
-        
-        unsigned int max = 0;
-        int getPosition(){
-            return current - topLine;
-        }
+        private:
+            int maxLines = 5;
+            int current = 0;
+            int topLine = 0;
+            bool selectionMade = false;
 
-        void moveDown(){
-            if (current != max - 1){
-                if (current == topLine + maxLines - 1){
-                    topLine ++;
+            unsigned int max = 0;
+            int getPosition(){
+                return current - topLine;
+            }
+
+            void moveDown(){
+                if (current != max - 1){
+                    if (current == topLine + maxLines - 1){
+                        topLine ++;
+                    }
+                    current ++;
                 }
-                current ++;
             }
-        }
-        void moveUp(){
-            if (current != 0){
-                if (current == topLine){
-                    topLine --;
+            void moveUp(){
+                if (current != 0){
+                    if (current == topLine){
+                        topLine --;
+                    }
+                    current --;
                 }
-                current --;
             }
-        }
-        int update(bool select, bool up, bool down){
-            if(select){
-                return current;
-            }
-            if(up){
-                moveUp();
-            } 
-            if(down){
-                moveDown();
-            }
-            ctrPrimary.Screen.clearScreen();
-            for (int i=0; i < maxLines; i++){
-                ctrPrimary.Screen.setCursor(i,2);
-                ctrPrimary.Screen.print("%s", text[i + topLine]);
-            }
-            ctrPrimary.Screen.setCursor(getPosition(),0);
-            ctrPrimary.Screen.print("->");
-            return -1;
-        }
-    public:
-        char text[8][32];
-        DisplaySelection(unsigned int maxOptions){
-            max = maxOptions;
-        }
-        int select(){
-            while(true){//repeat update until a selection is chosen
-                bool selectBtn = ctrPrimary.ButtonA.pressing();
-                bool up = ctrPrimary.ButtonUp.pressing();
-                bool down = ctrPrimary.ButtonDown.pressing();
-                int status = update(selectBtn, up, down);//call update function
-                while(ctrPrimary.ButtonA.pressing() || ctrPrimary.ButtonUp.pressing() || ctrPrimary.ButtonDown.pressing()){wait(20);}//wait for all buttons to be released
-                if (status != -1){//repeat loop until selection is made (update return something other than -1)
-                    return status;//return number selected
-                    break;
+            int update(bool select, bool up, bool down){
+                if(select){
+                    return current;
                 }
-                wait(20); //Update at 50 hertz
+                if(up){
+                    moveUp();
+                } 
+                if(down){
+                    moveDown();
+                }
+                robotMain.Screen.clearScreen();
+                for (int i=0; i < maxLines; i++){
+                    robotMain.Screen.setCursor(i+1,3);
+                    robotMain.Screen.print("%s", text[i + topLine]);
+                }
+                robotMain.Screen.setCursor(getPosition()+1,0);
+                robotMain.Screen.print("->");
+                return -1;
             }
-        }
-        
+        public:
+            char text[8][32];
+            DisplaySelection(unsigned int maxOptions){
+                max = maxOptions;
+            }
+            int select(){
+                while(true){//repeat update until a selection is chosen
+                    bool selectBtn = ctrPrimary.ButtonA.pressing();
+                    bool up = ctrPrimary.ButtonUp.pressing();
+                    bool down = ctrPrimary.ButtonDown.pressing();
+                    int status = update(selectBtn, up, down);//call update function
+                    while(ctrPrimary.ButtonA.pressing() || ctrPrimary.ButtonUp.pressing() || ctrPrimary.ButtonDown.pressing()){wait(20);}//wait for all buttons to be released
+                    if (status != -1){//repeat loop until selection is made (update return something other than -1)
+                        return status;//return number selected
+                        break;
+                    }
+                    wait(50); //Update at 20 hertz
+                }
+            }
+
 };
 class PromptClose {//Handles whether the user wants to exit at a particular screen
     private:
         bool prompt = false;//bool for whether the second confirm close screen should be shown
     public:
         int update(bool A, bool B){//A and B are the values for selecting the different options
-            ctrPrimary.Screen.clearLine(1);
-            ctrPrimary.Screen.clearLine(2);
+            robotMain.Screen.setCursor(1,0);
+            robotMain.Screen.clearLine();
+            robotMain.Screen.setCursor(2,0);
+            robotMain.Screen.clearLine();
             if((!prompt && B) || (prompt && !A && !B)){//Shows second screen if prompt is true or b is chosen on first screen
                 prompt = true;
-                ctrPrimary.Screen.setCursor(1,0);
-                ctrPrimary.Screen.print("(A) Close?");
-                ctrPrimary.Screen.newLine();
-                ctrPrimary.Screen.print("(B) Back");
+                robotMain.Screen.setCursor(1,0);
+                robotMain.Screen.print("(A) Close?");
+                robotMain.Screen.newLine();
+                robotMain.Screen.print("(B) Back");
                 return 0;
             }
             if((prompt && B) || (!prompt && !B)){//Shows first screen if prompt is false or if b is chosen on the second screen
                 prompt = false;
-                ctrPrimary.Screen.setCursor(1,0);
-                ctrPrimary.Screen.print("(B) Close");
+                robotMain.Screen.setCursor(1,0);
+                robotMain.Screen.print("(B) Close");
                 return 0;
             }
             return 1;//returns 0 if there is no close chosen, returns 1 if close is chosen
@@ -188,9 +190,11 @@ void colorSelect(){//method for selecting field color
     strcpy(selectColor.text[1], "Blue");
     colorRed = (selectColor.select() == 0);
 }
-int main(){//main control function
-    while(true){//repeat until close is selected
-        //Prompt mode selection
+int main() {
+    robotMain.Screen.setFont(vex::fontType::mono40);
+    robotMain.Screen.clearScreen();
+    robotMain.Screen.setCursor(1,0);
+    while(true){
         DisplaySelection selectMode = DisplaySelection(5); //Create Display object
         strcpy(selectMode.text[0], "Field Control");//set values in array to options
         strcpy(selectMode.text[1], "Skills Control");
@@ -202,14 +206,14 @@ int main(){//main control function
             calibrateGyros();//Calibrate gyro sensors
             colorSelect();//select team color
             int autonMode = selectAutonomous();//select auton to run
-
+            robotMain.Screen.clearScreen();
             while(true){//loop for competition
                 PromptClose promptExit = PromptClose();
-                ctrPrimary.Screen.setCursor(0,0);
-                ctrPrimary.Screen.clearLine();
-                ctrPrimary.Screen.print("FC-Disabled");
                 bool statusClose = false;
                 while(!compControl.isEnabled()){//While disabled, user has option to close field control 
+                    robotMain.Screen.setCursor(1,0);
+                    robotMain.Screen.clearLine();
+                    robotMain.Screen.print("FC-Disabled");
                     bool a = ctrPrimary.ButtonA.pressing();
                     bool b = ctrPrimary.ButtonB.pressing();
                     statusClose = (promptExit.update(a, b) == 1);
@@ -237,17 +241,14 @@ int main(){//main control function
         if(mode == 1){//Skills mode- same as field control, except no color selection
             colorRed = true;
             int autonMode = selectAutonomous();
-
+            robotMain.Screen.clearScreen();
             while(true){
                 bool statusClose = false;
                 PromptClose promptExit = PromptClose();
-                ctrPrimary.Screen.setCursor(0,0);
-                ctrPrimary.Screen.clearLine();
-                ctrPrimary.Screen.print("SK-Disabled");
                 while(!compControl.isEnabled()){
-                    ctrPrimary.Screen.setCursor(0,0);
-                    ctrPrimary.Screen.clearLine();
-                    ctrPrimary.Screen.print("SK-Disabled");
+                    robotMain.Screen.setCursor(1,0);
+                    robotMain.Screen.clearLine();
+                    robotMain.Screen.print("SK-Disabled");
                     bool a = ctrPrimary.ButtonA.pressing();
                     bool b = ctrPrimary.ButtonB.pressing();
                     statusClose = (promptExit.update(a, b) == 1);
@@ -275,11 +276,11 @@ int main(){//main control function
         if(mode == 2){//Auton testing mode
             while (true){
                 while(true){
-                    ctrPrimary.Screen.clearScreen();
-                    ctrPrimary.Screen.setCursor(0,0);
-                    ctrPrimary.Screen.print("Setup Robot");
-                    ctrPrimary.Screen.newLine();
-                    ctrPrimary.Screen.print("(A) Done");//Waits until robot is placed properly for auton
+                    robotMain.Screen.clearScreen();
+                    robotMain.Screen.setCursor(1,0);
+                    robotMain.Screen.print("Setup Robot");
+                    robotMain.Screen.newLine();
+                    robotMain.Screen.print("(A) Done");//Waits until robot is placed properly for auton
                     if (ctrPrimary.ButtonA.pressing()){
                         break;
                     }
