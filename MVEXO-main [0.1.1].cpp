9864c6e5fd9b78a9e42837f17fafd8d30ed214b2
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-15 18:07:53 -0400

    Update MVEXO-main [0.1.1].cpp

--- MVEXO-main [0.1.1].cpp
+++ MVEXO-main [0.1.1].cpp
@@ -2,7 +2,7 @@
 /*                    5249S                   */
 /*                The ManiVEXo                */
 /*                    Main                    */
-/*                Version 0.1.0               */
+/*                Version 0.1.1               */
 /*--------------------------------------------*/
 #include "robot-config.h"
 #include <cmath>
