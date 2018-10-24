5d09950d234ac71f9d1791b50f34844a88a752ca
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-23 21:53:27 -0400

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -10,7 +10,7 @@ vex::motor mtrLiftLeft = vex::motor(vex::PORT2, true);
 vex::motor mtrLiftRight = vex::motor(vex::PORT9);
 vex::gyro gyroNav = vex::gyro(robotMain.ThreeWirePort.A);
 
-vex::vision visLauncher = vex::vision(vex::PORT2);
+vex::vision visLauncher = vex::vision(vex::PORT3);
 vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherZ = vex::accelerometer(robotMain.ThreeWirePort.C);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.D);
