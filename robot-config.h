2c20b9e19cb108b4c6da95dca994e8eb35c84364
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-30 14:47:04 -0400

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -4,7 +4,6 @@ vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherZ = vex::accelerometer(robotMain.ThreeWirePort.C);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.D);
 
-vex::vision visLauncher = vex::vision(vex::PORT2);
 vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.E);
 vex::accelerometer accelLauncherX = vex::accelerometer(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.C);
