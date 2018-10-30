1c92b17ab2854a66f368c9edb4b1174c38fce01c
caek88 <35225849+caek88@users.noreply.github.com>
2018-10-30 14:58:36 -0400

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -1,11 +1,18 @@
 //robot-config.h
+//robot-config.h
+
+vex::brain robotMain;
+vex::controller ctrPrimary = vex::controller(vex::controllerType::primary);
+vex::competition compControl;
+
+vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
+vex::motor mtrDriveRight = vex::motor(vex::PORT10, true);
+vex::motor mtrLiftLeft = vex::motor(vex::PORT2, true);
+vex::motor mtrLiftRight = vex::motor(vex::PORT9);
 vex::vision visLauncher = vex::vision(vex::PORT3);
-vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.B);
-vex::accelerometer accelLauncherZ = vex::accelerometer(robotMain.ThreeWirePort.C);
-vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.D);
+vex::gyro gyroNav = vex::gyro(robotMain.ThreeWirePort.A);
 
 vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.E);
 vex::accelerometer accelLauncherX = vex::accelerometer(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.C);
 vex::accelerometer accelLauncherZ = vex::accelerometer(robotMain.ThreeWirePort.D);
-
