4df399c1d89ad4117257808b566cd2d9623cb338
caek88 <35225849+caek88@users.noreply.github.com>
2018-11-13 23:02:14 -0500

    Update robot-config.h

--- robot-config.h
+++ robot-config.h
@@ -1,5 +1,4 @@
 //robot-config.h
-//robot-config.h
 
 vex::brain robotMain;
 vex::controller ctrPrimary = vex::controller(vex::controllerType::primary);
@@ -12,7 +11,14 @@ vex::motor mtrLiftRight = vex::motor(vex::PORT9);
 vex::motor mtrClaw = vex::motor(vex::PORT4);
 vex::motor mtrLauncherAngle = vex::motor(vex::PORT5);
 vex::motor mtrLauncherFire = vex::motor(vex::PORT6);
-vex::vision visLauncher = vex::vision(vex::PORT7);
+vex::vision::signature SIG_FLAG_RED (1, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_FLAG_BLUE (2, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_3 (3, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_4 (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_5 (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_6 (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature SIG_7 (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision visLauncher (vex::PORT7, 50, SIG_FLAG_RED, SIG_FLAG_BLUE, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
 vex::gyro gyroNav = vex::gyro(robotMain.ThreeWirePort.A);
 
 vex::gyro gyroLauncher = vex::gyro(robotMain.ThreeWirePort.E);
