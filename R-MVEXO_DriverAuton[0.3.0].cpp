d3e03f87558812073e2be7f223f0341f18cc6995
caek88 <35225849+caek88@users.noreply.github.com>
2018-11-28 22:37:23 -0500

    Update R-MVEXO_DriverAuton[0.3.0].cpp

--- R-MVEXO_DriverAuton[0.3.0].cpp
+++ R-MVEXO_DriverAuton[0.3.0].cpp
@@ -9,11 +9,11 @@ void auton(int autonMode){
     ctrPrimary.Screen.clearScreen();
     ctrPrimary.Screen.setCursor(1,0);
     ctrPrimary.Screen.print("Autonomous");
-    if (autonMode == 1){
+    if (autonMode == 1){//Run Auton 1
         //Declare variable here
         robotMain.Screen.clearScreen();
         robotMain.Screen.setCursor(1,0);
-        int clock = 0;
+        int clock = 0;//Reset clock, motors, and process
         int process = 0; //variable to control where in the auton you are
         mtrDriveLeft.resetRotation();
         mtrDriveRight.resetRotation();
@@ -27,46 +27,46 @@ void auton(int autonMode){
                 robotMain.Screen.print("%d", clock);
                 clock = 0;
             }*/
-            if(process == 0){
-                if (driveToPoint(360, 0)){
-                    mtrDriveLeft.resetRotation();
+            if(process == 0){//Process 0
+                if (driveToPoint(360, 0)){//Wait until the robot drives 360 degrees
+                    mtrDriveLeft.resetRotation();//Reset motors
                     mtrDriveRight.resetRotation();
                     driveYawPID.reset();
                     driveSpeedPID.reset();
                     stopAllMotors();
-                    process++;
+                    process++;//Move to process 1
                 }
             };
-            if(process == 1){
-                if (pointTurn(650)){
-                    mtrDriveLeft.resetRotation();
+            if(process == 1){//Process 1
+                if (pointTurn(-650)){//Wait until the robot turns 90 degrees to the left
+                    mtrDriveLeft.resetRotation();//Reset motors
                     mtrDriveRight.resetRotation();
                     driveYawPID.reset();
                     driveSpeedPID.reset();
                     stopAllMotors();
-                    process++;
-                    wait(150);
+                    process++;//go to process 2
+                    wait(150);//Wait 150 mSec
                 }
             }
-            if (process == 2){
-                if (driveToPoint(360, 900)){
-                    process++;
+            if (process == 2){//Process 2
+                if (driveToPoint(360, -900)){//Wait until the robot drives 360 degrees forward
+                    process++;//go to process 3
                 }
                 robotMain.Screen.clearScreen();
                 robotMain.Screen.setCursor(1,0);
                 robotMain.Screen.print("Gyro: %d", gyroNav.value(vex::analogUnits::range12bit));
                 robotMain.Screen.newLine();
             }
-            runDiagnostics();
+            runDiagnostics();//display warnings
             wait(20);//run at 50 Hz
             clock ++;
         }
     }
-    if (autonMode == 2){
+    if (autonMode == 2){//Aligns ball launcher
         //Declare variable here
         robotMain.Screen.clearScreen();
         robotMain.Screen.setCursor(1,0);
-        int clock = 0;
+        int clock = 0;//Reset clock, motors, and process
         int process = 0;
         launchAnglePID.reset();
         BallLauncher targetSystem;
@@ -92,43 +92,63 @@ void driver(){
     //Declare variables here
     ctrPrimary.Screen.clearScreen();
     ctrPrimary.Screen.setCursor(0,0);
-    ctrPrimary.Screen.print("Party Time");
+    ctrPrimary.Screen.print("Party Time");//Party Time
     BallLauncher targetSystem;
-    int clock = 0;
-    bool mode6 = true;
+    bool autoTarget = false;
+    bool waitForReleaseA = false;
+    ctrPrimary.Screen.setCursor(3,0);
+    ctrPrimary.Screen.clearLine(3);
+    ctrPrimary.Screen.print("AutoTarget: %s", autoTarget?"On":"Off");
     while (confirmDriver()){
         //Run driver implementation here
-        int lightMode = targetSystem.scanForFlags();
-        double angle = targetSystem.targetSpecificFlag();
-        if (ctrPrimary.ButtonR1.pressing() || ctrPrimary.ButtonR2.pressing()){
-            robot.launchAngle(ctrPrimary.ButtonR1.pressing(), ctrPrimary.ButtonR2.pressing());
-        } else {
-            if (angle != -1 && !ctrPrimary.ButtonX.pressing()){
-                if(setLauncherToAngle(angle) < 1){
-                    if (lightMode == 5){
-                        if (clock == 500){
-                            clock = 0;
-                            mode6 = !mode6;
-                        }
-                        if (mode6){
-                            lightMode = 6;
-                        }
-                    }
-                }
+        if (ctrPrimary.ButtonA.pressing() && !waitForReleaseA){//Turns on auto target
+            waitForReleaseA = true;
+            autoTarget = !autoTarget;
+            ctrPrimary.Screen.setCursor(3,0);
+            ctrPrimary.Screen.clearLine(3);
+            if (autoTarget){//Display auto target state
+                ctrPrimary.Screen.print("AutoTarget: On");
             } else {
-                mtrLauncherAngle.stop(vex::brakeType::hold);
+                ctrPrimary.Screen.print("AutoTarget: Off");
             }
         }
-        clock += 20;
-        robot.launchFire(ctrPrimary.ButtonX.pressing());
-        robot.claw(ctrPrimary.ButtonL1.pressing(), ctrPrimary.ButtonL2.pressing());
-        int y = ctrPrimary.Axis3.position(vex::percentUnits::pct);
-        int x = ctrPrimary.Axis4.position(vex::percentUnits::pct);
-        robot.lift(ctrPrimary.Axis2.position(vex::percentUnits::pct));
-        robot.driveH(y, x);
-        runDiagnostics();
+        if (!ctrPrimary.ButtonA.pressing() && waitForReleaseA){//Waits for release to change the state of auto target
+            waitForReleaseA = false;
+        }
+        if (autoTarget){//Run limited chassis
+            targetSystem.scanForFlags();//Run target system
+            double angle = targetSystem.targetSpecificFlag();
+            if (!ctrPrimary.ButtonR1.pressing() && !ctrPrimary.ButtonR2.pressing()){//If no manual controls are being pressed, Angle the ball launcher
+                if (angle != -1 && !ctrPrimary.ButtonX.pressing()){
+                    setLauncherToAngle(angle);
+                } else {
+                    mtrLauncherAngle.stop(vex::brakeType::hold);
+                }
+            }
+            if (ctrPrimary.ButtonLeft.pressing()){//Run the chassis on limited controls
+                robot.driveH(0,-30);
+            } else {
+                if (ctrPrimary.ButtonRight.pressing()){
+                    robot.driveH(0,30);
+                } else {
+                    horizontalAlignFlag(targetSystem.closestPositionX);
+                }
+            }
+        } else {
+            int y = ctrPrimary.Axis3.position(vex::percentUnits::pct);//Run chassis with joystick
+            int x = ctrPrimary.Axis4.position(vex::percentUnits::pct);
+            robot.lift(ctrPrimary.Axis2.position(vex::percentUnits::pct));
+            robot.driveH(y, x);
+        }
+        robot.launchAngle(ctrPrimary.ButtonR1.pressing(), ctrPrimary.ButtonR2.pressing());//Angle launcher manually
+        robot.liftBall(ctrPrimary.ButtonUp.pressing(), ctrPrimary.ButtonDown.pressing());//Ball Lift
+        robot.launchFire(ctrPrimary.ButtonX.pressing());//Fire the launcher
+        robot.claw(ctrPrimary.ButtonL1.pressing(), ctrPrimary.ButtonL2.pressing());//Run the claw
+        runDiagnostics();//Check for warnings and display
+        robotMain.Screen.clearScreen();//Display the gyros value on the processor
+        robotMain.Screen.setCursor(1,0);
+        robotMain.Screen.print("Gyro: %d", gyroNav.value(vex::analogUnits::range12bit));
+        robotMain.Screen.newLine();
         wait(20);//run at 50 Hz
-        
     }
-    
 }
