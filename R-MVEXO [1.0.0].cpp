b99ddb73dffc2dc905e62ddab1261712129df5bb
caek88 <35225849+caek88@users.noreply.github.com>
2019-01-23 16:44:40 -0500

    Update R-MVEXO [1.0.0].cpp

--- R-MVEXO [1.0.0].cpp
+++ R-MVEXO [1.0.0].cpp
@@ -5,14 +5,14 @@ vex::controller ctrPrimary = vex::controller(vex::controllerType::primary);
 vex::controller ctrSecond = vex::controller(vex::controllerType::partner);
 vex::competition compControl;
 
-vex::motor mtrDriveLeft = vex::motor(vex::PORT12);
-vex::motor mtrDriveRight = vex::motor(vex::PORT19, true);
-vex::motor mtrLiftLeft = vex::motor(vex::PORT17, true);
-vex::motor mtrLiftRight = vex::motor(vex::PORT2);
-vex::motor mtrClaw = vex::motor(vex::PORT15, true);
-vex::motor mtrLauncherAngle = vex::motor(vex::PORT7, true);
-vex::motor mtrLauncherFire = vex::motor(vex::PORT6);
-vex::motor mtrBallLift = vex::motor(vex::PORT4, true);
+vex::motor mtrDriveLeft = vex::motor(vex::PORT1);
+vex::motor mtrDriveRight = vex::motor(vex::PORT7, true);
+vex::motor mtrLiftLeft = vex::motor(vex::PORT3, true);
+vex::motor mtrLiftRight = vex::motor(vex::PORT4);
+vex::motor mtrClaw = vex::motor(vex::PORT20, true);
+vex::motor mtrLauncherAngle = vex::motor(vex::PORT10, true);
+vex::motor mtrLauncherFire = vex::motor(vex::PORT8);
+vex::motor mtrBallLift = vex::motor(vex::PORT5, true);
 
 vex::vision::signature SIG_FLAG_RED (1, 7079, 7977, 7528, 409, 1415, 912, 8.199999809265137, 0);
 vex::vision::signature SIG_FLAG_BLUE (2, -4121, -3129, -3625, 13045, 15237, 14141, 9.300000190734863, 0);
@@ -21,7 +21,7 @@ vex::vision::signature SIG_4 (4, 0, 0, 0, 0, 0, 0, 3, 0);
 vex::vision::signature SIG_5 (5, 0, 0, 0, 0, 0, 0, 3, 0);
 vex::vision::signature SIG_6 (6, 0, 0, 0, 0, 0, 0, 3, 0);
 vex::vision::signature SIG_7 (7, 0, 0, 0, 0, 0, 0, 3, 0);
-vex::vision visLauncher (vex::PORT5, 61, SIG_FLAG_RED, SIG_FLAG_BLUE, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
+vex::vision visLauncher (vex::PORT11, 61, SIG_FLAG_RED, SIG_FLAG_BLUE, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
 
 vex::accelerometer accelLauncherX = vex::accelerometer(robotMain.ThreeWirePort.B);
 vex::accelerometer accelLauncherY = vex::accelerometer(robotMain.ThreeWirePort.C);
