b6a7e4c724a0ad747c8e247f300ca300bc6e9c8a
caek88 <35225849+caek88@users.noreply.github.com>
2019-01-30 16:10:27 -0500

    Update InitialVelocity.cpp

--- InitialVelocity.cpp
+++ InitialVelocity.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+int main(){
+    double distance = 0;
+    double height = 0;
+    double angle = 0;
+    cout << "Initial Velocity Calculator for 5249S\nInput Firing Angle(degrees): ";
+    cin >> angle;
+    cout << "\nInput X distance(meters): ";
+    cin >> distance;
+    cout << "\nInput Y height(meters): ";
+    cin >> height;
+    angle *= (3.1415/180);
+    double velocity = sqrt((-9.8*pow(distance, 2)/(2 * pow(cos(angle), 2) * (height - distance * tan(angle))));
+    cout << "\n" << velocity;
+    return 0;
+}
